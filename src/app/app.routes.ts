31c42fbe2cea5f3638333dac19f391877ae0a2f5
Zensi77 <juanmaespi7@gmail.com>
2024-11-27 00:02:03 +0100

    Estructura de directorios

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,12 +3,12 @@ import { Routes } from '@angular/router';
 export const routes: Routes = [
   {
     path: '',
-    title: 'inicio',
+    title: 'Inicio',
     loadComponent: () => import('./dashboard/dashboard.component').then(m => m.DashboardComponent),
     children: [
       {
         path: 'introduccion',
-        title: 'introducción',
+        title: 'Introducción',
         loadComponent: () => import('./dashboard/pages/initial-page/initial-page.component').then(m => m.InitialPageComponent),
       }
     ]
---
+++ src/app/dashboard/pages/animations-page/animations-page.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  standalone: true,
+  imports: [],
+  templateUrl: './animations-page.component.html',
+  styles: ``
+})
+export class AnimationsPageComponent {
+
+}
