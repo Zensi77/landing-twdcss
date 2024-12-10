e70cef01e5bb1b022e84ca4da690d4456568a812
Zensi77 <juanmaespi7@gmail.com>
2024-12-10 18:28:24 +0100

    Fin exposicion

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,16 @@
 module.exports = {
   content: ["./src/**/*.{html,ts}"],
   theme: {
-    extend: {},
-    
+    extend: {
+      colors: {
+        primary: "#1E40AF", // Sobreescribe o añade colores personalizados.
+        secondary: "#D97706",
+      },
+      fontFamily: {
+        sans: ["Inter", "sans-serif"], // Cambia las fuentes.
+        serif: ["Merriweather", "serif"],
+      },
+    },
   },
   plugins: [require("tailwindcss-textshadow")],
 };
